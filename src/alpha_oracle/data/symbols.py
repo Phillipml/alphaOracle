36222c167aa4be9c63606b7518ad5c7b03bef653
Phillipml <contato.phillip.menezes@gmail.com>
2026-02-13 10:56:54 -0300

    feat: BR market symbols configuration

---
+++ src/alpha_oracle/data/symbols.py
@@ -0,0 +1,30 @@
+BR_SYMBOLS = {
+    "indices": [
+        "^BVSP",  # IBOVESPA
+    ],
+    "blue_chips": [
+        "PETR4.SA",   # Petrobras PN
+        "VALE3.SA",   # Vale ON
+        "ITUB4.SA",   # Itaú PN
+        "BBDC4.SA",   # Bradesco PN
+        "ABEV3.SA",   # Ambev
+        "WEGE3.SA",   # WEG
+        "RENT3.SA",   # Localiza
+        "BBAS3.SA",   # Banco do Brasil
+        "RAIL3.SA",   # Rumo (logística)
+        "ELET3.SA",   # Eletrobras
+    ],
+    "diversificacao": [
+        "MGLU3.SA",   # Varejo
+        "VIVT3.SA",   # Telecom
+        "CSNA3.SA",   # Siderurgia
+        "CYRE3.SA",   # Construção
+        "SBSP3.SA",   # Saneamento
+        "EQTL3.SA",   # Energia
+    ],
+}
+ALL_BR_SYMBOLS = (
+    BR_SYMBOLS["indices"] 
+    + BR_SYMBOLS["blue_chips"] 
+    + BR_SYMBOLS["diversificacao"]
+)
